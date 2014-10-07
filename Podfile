d681111c538a1b88fc4d7d4e06952529cc57f35d
Kevin Chen <xmkevinchen@gmail.com>
2014-10-07 11:41:43 -0700

    - Initialize project

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment this line to define a global platform for your project
+
+platform :ios, "7.0"
+inhibit_all_warnings!
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+target "TicTacToe" do
+	pod 'FontAwesomeKit', '~> 2.1.0'
+end
+
+target "TicTacToeTests" do
+
+end
+
---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Kevin Chen on 10/6/14.
+//  Copyright (c) 2014 Kevin Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
